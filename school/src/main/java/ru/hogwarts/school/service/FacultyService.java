cbff68123804f7a220a359eac12175ac4d53a9d0
Pavlen <pavlenovm@inbox.ru>
2026-06-21 15:03:00 +0300

    Add package service and model

---
+++ school/src/main/java/ru/hogwarts/school/service/FacultyService.java
@@ -0,0 +1,39 @@
+package ru.hogwarts.school.service;
+
+import ru.hogwarts.school.model.Faculty;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FacultyService {
+    private Map<Long, Faculty> facultys = new HashMap<>();
+    private Long idCounter = 1L;
+
+    // CRUD методы
+    public Faculty create(Faculty faculty) {
+        facultys.get(idCounter++);
+        facultys.put(faculty.getId(), faculty);
+        return faculty;
+    }
+
+    public Faculty get(Long id) {
+        return facultys.get(id);
+    }
+
+    public Faculty update(Long id, Faculty updatedStudent) {
+        if (facultys.containsKey(id)) {
+            updatedStudent.setId(id);
+            facultys.put(id, updatedStudent);
+            return updatedStudent;
+        }
+        return null;
+    }
+
+    public void delete(Long id) {
+        facultys.remove(id);
+    }
+
+    public Map<Long, Faculty> getAll() {
+        return facultys;
+    }
+}
